f6d2cdc67cb8ddc9a979da5947627c5c6e630a2e
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-08-02 14:14:14 -0400

    Starting to generate conda package for minc-toolkit(s)

---
+++ minc-toolkit-v2/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mkdir build && cd build
+# assume correct compilers will be setup by environment
+# don't build visual tools though
+
+cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
+      -DMT_BUILD_ITK_TOOLS:BOOL=ON \
+      -DMT_BUILD_SHARED_LIBS:BOOL=ON \
+      -DMT_BUILD_VISUAL_TOOLS:BOOL=OFF \
+      -DUSE_SYSTEM_OPENJPEG:BOOL=OFF \
+      -DMT_BUILD_ANTS:BOOL=ON \
+      -DMT_BUILD_C3D:BOOL=ON
+      -DMT_BUILD_ELASTIX:BOOL=ON \
+      -DMT_BUILD_OPENBLAS:BOOL=ON \
+      -DMT_BUILD_SHARED_LIBS:BOOL=ON \
+      -DCMAKE_Fortran_FLAGS:STRING="" \
+      -DCMAKE_CXX_FLAGS:STRING="" \
+      -DCMAKE_C_FLAGS:STRING=""
+
+make install
