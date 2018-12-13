ff31e9177f8e945ee791f5c441768b64b17187d5
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-12-12 19:11:38 -0500

    Adding falcon building scripts

---
+++ falcon/build.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+set -e -x
+
+mkdir -p build && cd build
+# assume correct compilers will be setup by environment
+# don't build visual tools though
+# use external cache directory
+export CMAKE_PREFIX_PATH=${CONDA_PREFIX}
+
+# default configuration , trying to use as many prebuilt packages as possible
+
+if [[ -z ${MACOSX_DEPLOYMENT_TARGET} ]];then
+    # building on linux
+    # HACK to make cmake be able to find libm
+    ln -sf ${CONDA_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/lib/libm.so.6 ${CONDA_PREFIX}/lib/libm.so
+    export CMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib:${CONDA_PREFIX}/lib64:${CONDA_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/lib
+else # building on MacOSX
+    export CMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib:${CONDA_PREFIX}/lib64:
+fi
+
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DBUILD_NVK:BOOL=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DHAVE_POVRAY:BOOL=ON \
+      -DMINC_TOOLKIT_DIR:PATH=${CONDA_PREFIX} \
+      -DLIBMINC_DIR:PATH=${CONDA_PREFIX}/lib \
+      -DTIFF_INCLUDE_DIR:PATH=${CONDA_PREFIX}/lib \
+      -DTIFF_LIBRARY_RELEASE:FILEPATH=${CONDA_PREFIX}/lib/libtiff.so
+
+if [[ -z ${MACOSX_DEPLOYMENT_TARGET} ]];then
+    cmake ..  -DMT_USE_OPENMP:BOOL=ON
+else
+    # OpenMP is not available on MacOSX
+    cmake ..  -DMT_USE_OPENMP:BOOL=OFF
+fi
+
+# build and install
+make -j${CPU_COUNT} && make install #
+
+#create environment activation & deactivation
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/minc-toolkit-v2-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/minc-toolkit-v2-deactivate.sh
---
+++ falcon/scripts/activate.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+BINDIR=/home/vfonov/src/build/falcon/src
+MODELDIR=/home/vfonov/src/falcon/data/icbm152_model_09c
+MODELDIR=$HOME/src/falcon/data 
