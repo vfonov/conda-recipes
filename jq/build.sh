1dffe1d671801f6b8c4c6d860a8abd9e01e02fc5
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-01-06 01:26:43 -0500

    Re-enabled oniguruma on MacOSX

--- jq/build.sh
+++ jq/build.sh
@@ -6,7 +6,7 @@ ls -l
 
 chmod +x configure
 
-./configure --with-oniguruma=$PREFIX --prefix=$PREFIX
+./configure --with-oniguruma=$PREFIX --disable-maintainer-mode --prefix=$PREFIX
 # builtin
 # build and install
 make -j${CPU_COUNT} && make check && make install #
--- oniguruma/build.sh
+++ oniguruma/build.sh
@@ -7,7 +7,7 @@ ls -l
 chmod +x configure
 
 ./configure --disable-maintainer-mode --prefix=$PREFIX
-# --with-oniguruma=builtin
+
 # build and install
 make -j${CPU_COUNT} && make check && make install #
 
