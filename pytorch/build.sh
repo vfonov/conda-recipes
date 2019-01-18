c7ebb008ba8641536cea2f75c7c7922dc50af195
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-01-18 13:33:16 -0500

    Added custom builds of pytorch and torchvision

--- pytorch/build.sh
+++ pytorch/build.sh
@@ -6,6 +6,7 @@ export CMAKE_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include:$CMAKE_LIBRARY_PATH
 export CMAKE_PREFIX_PATH=$PREFIX
 export PYTORCH_BUILD_VERSION=$PKG_VERSION
 export PYTORCH_BUILD_NUMBER=$PKG_BUILDNUM
+
 if [ ${cudatoolkit} == "8.0" ]; then
     # compile for Kepler, Kepler+Tesla, Maxwell, Pascal
     export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1"
@@ -15,21 +16,28 @@ if [ ${cudatoolkit} == "9.0" ]; then
     export TORCH_CUDA_ARCH_LIST="3.0;3.5;5.0;5.2+PTX;6.0;6.1;7.0"
 fi
 export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
-# use conda provided nccl
-export NCCL_ROOT_DIR="${PREFIX}"
-export NCCL_LIB_DIR="${PREFIX}/lib"
-export NCCL_INCLUDE_DIR="${PREFIX}/include"
-export TH_BINARY_BUILD=1
-export USE_SYSTEM_NCCL=1
-export USE_CUDA=1
-export CUDA_HOME=${PREFIX}
-export CUDA_TOOLKIT_ROOT_DIR=${PREFIX}
 
-export MAGMA_HOME="${PREFIX}"
+# Disabling CUDA and DISTRIBUTED
+
+# use conda provided nccl
+#export NCCL_ROOT_DIR="${PREFIX}"
+#export NCCL_LIB_DIR="${PREFIX}/lib"
+#export NCCL_INCLUDE_DIR="${PREFIX}/include"
+export TH_BINARY_BUILD=1
+export USE_SYSTEM_NCCL=0
+export USE_CUDA=OFF
+export USE_MKL=OFF
+export USE_OPENMP=ON
+
+export NO_CUDA=1
+#export NO_DISTRIBUTED=OFF
+
+#export CUDA_HOME=${PREFIX}
+#export CUDA_TOOLKIT_ROOT_DIR=${PREFIX}
+
+#export MAGMA_HOME="${PREFIX}"
 export PYTORCH_BINARY_BUILD=1
 export LIBRT=$(find ${BUILD_PREFIX} -name "librt.so")
-
-
 
 
 python setup.py install
