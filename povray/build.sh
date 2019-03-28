d4b3f0bc28c04810fda01f2121fb4460fef34f0f
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2019-03-28 09:45:56 -0400

    Disabled cpu-specific optimization to build compatible binaries

--- povray/build.sh
+++ povray/build.sh
@@ -16,24 +16,28 @@ export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
 #export MACOSX_DEPLOYMENT_TARGET="10.9"
 export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
 
-./configure --prefix=$PREFIX --without-x \
+./configure --prefix=$PREFIX \
+  --without-x \
   --with-zlib=${CONDA_PREFIX} \
   --with-libpng=${CONDA_PREFIX} \
   --with-libjpeg=${CONDA_PREFIX} \
   --with-libtiff=${CONDA_PREFIX} \
   --with-boost=${CONDA_PREFIX} \
   --with-boost-libdir=${CONDA_PREFIX}/lib \
+  --disable-optimiz-arch \
  COMPILED_BY="Vladimir S. FONOV <vladimir.fonov@gmail.com>"
 else
 #  export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 # linux
-./configure --prefix=$PREFIX --without-x \
+./configure --prefix=$PREFIX \
+  --without-x \
   --with-zlib=${CONDA_PREFIX} \
   --with-libpng=${CONDA_PREFIX} \
   --with-libjpeg=${CONDA_PREFIX} \
   --with-libtiff=${CONDA_PREFIX} \
   --with-boost=${CONDA_PREFIX} \
   --with-boost-libdir=${CONDA_PREFIX}/lib \
+  --disable-optimiz-arch \
  COMPILED_BY="Vladimir S. FONOV <vladimir.fonov@gmail.com>"
 
 fi
